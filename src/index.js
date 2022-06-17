6f3a3396bd85a065319ff5d7174797c8188e1cfd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-17 06:41:48 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createToDo(e.target["new-task-description"].value)
+  })
+  function createToDo(input) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', performDelete)
+    btn.textContent = 'Delete To Do list'
+    p.textContent = `${input} `
+    p.appendChild(btn)
+    document.getElementById('tasks').appendChild(p)
+  }
+
+  function performDelete(e) {
+    e.target.parentNode.remove()
+  }
+
 });
+
+
+
